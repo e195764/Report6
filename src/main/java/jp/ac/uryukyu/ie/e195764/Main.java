41f3afee37f8141a09f0971e6b4986dc2e032c80
Wataru Kinjo <e195764@ie.u-ryukyu.ac.jp>
2020-01-21 04:05:35 +0900

    Reviewクラスの実装

--- src/main/java/jp/ac/uryukyu/ie/e195764/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195764/Main.java
@@ -4,24 +4,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("選択してください。");
-        System.out.println("・英単語テストをする。");
-        System.out.println("・英単語を復讐する。");
-        System.out.println("・リストに単語を追加する。");
+        System.out.println("・英単語テストをする ---> a");
+        System.out.println("・英単語を復習する ---> b");
+        System.out.println("・リストに単語を追加する ---> c");
         System.out.println("（入力待ち）");
+
+
         Scanner in = new Scanner(System.in);
         String a;
         a = in.nextLine();
 
+        Review review = new Review();
 
-        if (a == "・英単語テストをする。") {
+
+        if (a.equals("a")) {
             //実装
-            return;
-        } else if (a == "・英単語を復讐する。") {
+
+
+        } else if (a.equals("b")) {
             //実装
-            return;
-        } else if (a == "・リストに単語を追加する・") {
+            for(int i = 0; i < review.words.size(); i++) {
+                System.out.println(review.words.get(i));
+            }
+
+        } else if (a.equals("c")) {
             //実装
-            return;
+
+
         } else {
             System.out.println("正しく入力してください。");
         }
---
+++ src/main/java/jp/ac/uryukyu/ie/e195764/Review.java
@@ -0,0 +1,19 @@
+package jp.ac.uryukyu.ie.e195764;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Review {
+    ArrayList<String> words = new ArrayList<>(Arrays.asList(
+            "merchant\t商人",
+            "needle\t針",
+            "oxygen\t酸素",
+            "diplomacy\t外交",
+            "rainfall\t降雨",
+            "structure\t構造",
+            "herbivore\t草食動物",
+            "lava\t溶岩",
+            "investment\t投資",
+            "fireplace\t暖炉"));
+
+
+}
